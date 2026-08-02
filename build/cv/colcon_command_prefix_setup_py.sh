60c869cbe8b1367ab653e1b08179de519e78e6ce
kushagra77 <kushagrazaveri@gmail.com>
2026-08-02 17:06:09 +1000

    fixed build errors

--- build/cv/colcon_command_prefix_setup_py.sh
+++ build/cv/colcon_command_prefix_setup_py.sh
@@ -1,2 +1,3 @@
 # generated from colcon_core/shell/template/command_prefix.sh.em
 . "/home/kushion/Documents/OpenMantaClaus/install/interfaces/share/interfaces/package.sh"
+. "/home/kushion/Documents/OpenMantaClaus/install/cv/share/cv/package.sh"
